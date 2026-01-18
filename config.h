341bc00d1ea22e1880538a60d8c7c5ac2a61ece9
amitshky <amitshkymail@gmail.com>
2026-01-18 17:32:38 +0000

    brightness script

--- config.h
+++ config.h
@@ -20,8 +20,9 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X) \
-	X(" ", "$HOME/dev/config/scripts/linux/volumePercent.sh",  0,  10) \
-	X(" ", "$HOME/dev/config/scripts/linux/batteryPercent.sh", 30, 5) \
-	X("󰃰 ", "$HOME/dev/config/scripts/linux/date.sh",           60, 0)
+	X("󰃝 ", "$HOME/dev/config/scripts/linux/brightnessPercent.sh",  0,  9) \
+	X(" ", "$HOME/dev/config/scripts/linux/volumePercent.sh",      0,  10) \
+	X(" ", "$HOME/dev/config/scripts/linux/batteryPercent.sh",     30, 5) \
+	X("󰃰 ", "$HOME/dev/config/scripts/linux/date.sh",               1,  8)
 
 #endif  // CONFIG_H
